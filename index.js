4696e2fbc914ab5fa740167007157b7bbbac038f
Sudana smp !! <sudanasmp@gmail.com>
2025-09-10 21:22:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function createBot() {
 
   const bot = mineflayer.createBot({
     host: 'puttur_smp.aternos.me',
-    port: 27124,
+    port: 37185,
     username: baseUsername,
     version: '1.16.5',
   });
